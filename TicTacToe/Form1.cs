26014b173f53ea6c0ce318f064cf87f7a273bee4
Enisuzunn <enisbesiktasli@gmail.com>
2024-11-12 23:50:38 +0300

    FirstCommit

--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -10,6 +10,7 @@ namespace TicTacToe
         Random random = new Random();
         int kazananOyuncu = 0;
         int kazananPc = 0;
+        List<Button> list;
 
         public Form1()
         {
@@ -19,17 +20,35 @@ namespace TicTacToe
 
         private void CPUtasima(object sender, EventArgs e)
         {
-
+            if (list.Count >0)
+            { 
+                int index = random.Next(list.Count);
+                list[index].Enabled = false;
+                currentPlayer = Player.O;
+                list[index].Text = currentPlayer.ToString();
+                list[index].BackColor = Color.DarkSalmon;
+                list.RemoveAt(index);
+                CheckGame();
+                gameTimer.Stop();
+            }
         }
 
         private void playerclickbutton(object sender, EventArgs e)
         {
+            var button = (Button)sender;
+            currentPlayer = Player.X;
+            button.Text = currentPlayer.ToString();
+            button.Enabled = false;
+            button.BackColor = Color.Cyan;
+            list.Remove(button);
+            CheckGame();
+            gameTimer.Start();
 
         }
 
         private void restartgame(object sender, EventArgs e)
         {
-
+            RestartGame();
         }
         private void CheckGame()
         {
@@ -37,7 +56,7 @@ namespace TicTacToe
         }
         private void RestartGame()
         {
-            List<Button> list = new List<Button>{ button1,button2,button3,button4,button5,button6,button7,button8,button9};
+            list = new List<Button>{ button1,button2,button3,button4,button5,button6,button7,button8,button9};
             foreach (Button button in list) {
                 button.Enabled = true;
                 button.Text = "?";
